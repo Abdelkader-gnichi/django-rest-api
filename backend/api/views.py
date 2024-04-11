5ec9c945924e581880310e061c37a9b59ab0d2ae
Abdelkader-gnichi <ggnichi097@gmail.com>
2024-04-11 13:50:31 +0100

    Rest Framework View & Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,29 +1,20 @@
-from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
-import json
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from products.models import Product
 from django.forms.models import model_to_dict
 # Create your views here.
 
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
-
+    '''\
+       this is a DRF API view
+    '''
     random_product = Product.objects.all().order_by('?').first()
-
     data= {}
     if random_product:
         data = model_to_dict(random_product, fields=['id','price'])
 
-    return JsonResponse(data)
+    return Response(data)
 
-    '''\
-        all this commented part will cause an error because here i want to convert 
-        the data (which is a dict) variable into a json string type in order to send it as a 
-        HttpResponse to my client, even changing the header type manually to application/json will 
-        not solve the problem.
-    '''
-        # print(data)
-        # data = dict(data) # this will not gonna help because the data var is actually a dict 
-        # data = json.dumps(data) # convert the dict into json string type
-
-    # return HttpResponse(data, headers={"content-type":'application/json'})
+    
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,16 +4,17 @@ import requests
 # endpoint = 'http://127.0.0.1:8000/api/?abc=abc' 
 endpoint = 'http://127.0.0.1:8000/api/'
 
-response = requests.get(endpoint,params={'abc':'abc'}, json={'query':'hello api'})
+response = requests.get(endpoint, params={'abc':'abc'}, json={'query':'hello api'})
 # json={'query':'hello api'} -> data 
 # data={'query':'hello api'} -> form data
 
-# print(response.headers)
-# print(response.text) # print rax text response
+print(response.headers)
+print(response.text) # print rax text response
 
 # Http Request -> HTML
 # Rest API Http Request -> JSON
 # JavaScript Object Notation ~ Python Dict
 
-# print(response.status_code)
+
+print(response.status_code)
 print(response.json())
