22363e8bdcf922b1ae2c1adaa601ef87ad24363d
Abdelkader-gnichi <ggnichi097@gmail.com>
2024-04-11 10:00:12 +0100

    adding some useful comments to the basic.py py_client file

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,14 @@ import requests
 endpoint = 'http://127.0.0.1:8000/api/'
 
 response = requests.get(endpoint,params={'abc':'abc'}, json={'query':'hello api'})
+# json={'query':'hello api'} -> data 
+# data={'query':'hello api'} -> form data
 
-print(response.text)
+print(response.text) # print rax text response
+
+# Http Request -> HTML
+# Rest API Http Request -> JSON
+# JavaScript Object Notation ~ Python Dict
+
 print(response.status_code)
 print(response.json())
