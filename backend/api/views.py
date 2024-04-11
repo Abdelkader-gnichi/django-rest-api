847d7bde66859efa921eb5fbfff238b585391e68
Abdelkader-gnichi <ggnichi097@gmail.com>
2024-04-11 12:55:02 +0100

    Django Model Instance Into a Dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
 from products.models import Product
+from django.forms.models import model_to_dict
 # Create your views here.
 
 def api_home(request, *args, **kwargs):
@@ -10,14 +11,19 @@ def api_home(request, *args, **kwargs):
 
     data= {}
     if random_product:
-        data['id'] = random_product.id
-        data['title'] = random_product.title
-        data['content'] = random_product.content
-        data['price'] = random_product.price
+        data = model_to_dict(random_product, fields=['id','price'])
 
-    # model instance (random_product)
-    # put its content into a dict | turn it into a dict
-    # return JSON response contains that dict to my client
-    
     return JsonResponse(data)
+
+    '''\
+        all this commented part will cause an error because here i want to convert 
+        the data (which is a dict) variable into a json string type in order to send it as a 
+        HttpResponse to my client, even changing the header type manually to application/json will 
+        not solve the problem.
+    '''
+        # print(data)
+        # data = dict(data) # this will not gonna help because the data var is actually a dict 
+        # data = json.dumps(data) # convert the dict into json string type
+
+    # return HttpResponse(data, headers={"content-type":'application/json'})
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -8,11 +8,12 @@ response = requests.get(endpoint,params={'abc':'abc'}, json={'query':'hello api'
 # json={'query':'hello api'} -> data 
 # data={'query':'hello api'} -> form data
 
-print(response.text) # print rax text response
+# print(response.headers)
+# print(response.text) # print rax text response
 
 # Http Request -> HTML
 # Rest API Http Request -> JSON
 # JavaScript Object Notation ~ Python Dict
 
-print(response.status_code)
+# print(response.status_code)
 print(response.json())
