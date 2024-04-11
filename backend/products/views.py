d4f9b340c69894051eb3423acd0f7ea4b67de09f
Abdelkader-gnichi <ggnichi097@gmail.com>
2024-04-11 18:01:28 +0100

    Django Rest Framework Generics RetriveAPIView (it's like get_product() MBV)

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all() # instances that you want to deals with
+    serializer_class = ProductSerializer # the serializer that will be used 
+
+    lookup_field = 'pk' # this how to specify the lookup column in the DB
+    
